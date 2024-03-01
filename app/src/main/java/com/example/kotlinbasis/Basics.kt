4ada451e851b47dcf5ee8926f0c426d375a9d1ca
WelingtonCarlos <wcfilho98@hotmail.com>
2024-03-01 17:05:54 -0300

    first commit

---
+++ app/src/main/java/com/example/kotlinbasis/Basics.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasis
+
+fun main() {
+
+    var mutable = 25
+    println(mutable)
+    mutable = 26
+    println(mutable)
+
+}
---
+++ app/src/main/java/com/example/kotlinbasis/firstExercise.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinbasis
+
+fun main () {
+    val constante = "Welington"
+    var mutable = 25
+
+    println("$constante tem $mutable anos!")
+
+    var mutableString = 26
+
+    println("$constante tem $mutable anos!")
+
+}
